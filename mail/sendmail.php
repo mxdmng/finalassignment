4532d1471f00a2a2a9e43c6d36fe10fae5de1426
M Domingo <micahdomingo@DM.local>
2016-12-05 22:49:43 -0500

    style changes

--- mail/sendmail.php
+++ mail/sendmail.php
@@ -3,22 +3,22 @@ require 'phpmailer/PHPMailerAutoload.php';
 
 //sendmail();
 
-function sendmail($from = "ammar@alqaraghuli.com",
+function sendmail($from = "mxdmng@gmail.com",
 	$subject="Contact message from your website", 
 	$body="message body",
-	$to="humber@alqaraghuli.com")
+	$to="mxdmng@gmail.com")
 {   //function sendmail
 	$mail = new PHPMailer;
 	$mail->isSMTP();            // Set mailer to use SMTP
 	$mail->Host = 'mail.alqaraghuli.com';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;             // Enable SMTP authentication
-	$mail->Username = 'humber@alqaraghuli.com';                 // SMTP username
-	$mail->Password = 'Humber2016';                           // SMTP password
+	$mail->Username = '';                 // SMTP username
+	$mail->Password = '';                           // SMTP password
 	$mail->Port = 25;     //587                               // TCP port to connect to 
-	$mail->setFrom('humber@alqaraghuli.com', 'Mailer');
+	$mail->setFrom('mxdmng@gmail.com', 'Mailer');
 	$mail->addAddress($to);     // Add a recipient, Name is optional
 	$mail->addReplyTo($from);
-	$mail->addCC('humber@alqaraghuli.com');
+	$mail->addCC('mxdmng@gmail.com');
 	$mail->isHTML(true);      // Set email format to HTML
 	$mail->Subject = $subject;
 	$mail->Body    = $body;
